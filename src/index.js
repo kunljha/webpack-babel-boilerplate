24194d850f452df33c9bb4092c4be331a04c2427
kunljha <jha711kunal@gmail.com>
2021-08-13 23:29:00 +0530

    babel-polyfill package installed to support async-await

--- src/index.js
+++ src/index.js
@@ -1,8 +1,2 @@
-// import './utilities'
-import otherSquare, { add, name } from './utilities.js'
-import otherScream from './scream.js'
-
-console.log('index.js')
-console.log(add(12, 5))
-console.log(otherSquare(8))
-console.log(otherScream(name))
+// entry file for webpack and app functionality is defined here
+console.log('boilerplate for building apps with webpack and babel')
--- src/scream.js
+++
@@ -1,5 +0,0 @@
-const scream = (str) => `${str.toUpperCase()}!`
-
-console.log('js module system is useful!')
-
-export { scream as default }
